4f892b913f13e645f61af2e869ae18af32ffb0f2
mvstudent12 <resident0000m@gmail.com>
2025-05-13 13:22:26 -0500

    added service-details page

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI);
+// mongoose.connect(process.env.MONGO_URI);
 
 //handlebars helpers
 const helpers = require("./app/helpers/helpers");
