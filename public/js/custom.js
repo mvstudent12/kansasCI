baf455fa5d675f7ea0a3afc4fd7bf5d84d8acc5f
mvstudent12 <residentIm12@gmail.com>
2025-08-29 09:59:12 -0500

    Prepare for full overwrite of remote repo

--- public/js/custom.js
+++ public/js/custom.js
@@ -394,7 +394,7 @@ $(document).ready(function () {
       columnDefs: [
         {
           orderable: false,
-          targets: [, 0, 6, -1],
+          targets: [0, 6, -1],
         },
       ],
       language: {
